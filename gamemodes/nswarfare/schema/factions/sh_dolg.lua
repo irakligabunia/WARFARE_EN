9764f8fe17165e6ac6f9880160aedd369ac555d1
stalkerino <66872447+irakligabunia@users.noreply.github.com>
2020-11-08 00:26:01 +0400

    Update sh_dolg.lua

--- gamemodes/nswarfare/schema/factions/sh_dolg.lua
+++ gamemodes/nswarfare/schema/factions/sh_dolg.lua
@@ -1,5 +1,5 @@
-FACTION.name = "Долг"
-FACTION.desc = "Долг — военизированная группировка, образованная бывшими военными."
+FACTION.name = "Duty"
+FACTION.desc = "Duty — Consisting of ex-servicemen of Ukraine, one of the most powerful factions in the Zone."
 FACTION.color = Color(255, 0, 0)
 FACTION.models = {
 	"models/hdmodels/kek1ch/stalker_dolg_0.mdl"
