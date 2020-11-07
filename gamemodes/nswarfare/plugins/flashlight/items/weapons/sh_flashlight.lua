2b99e98fc70d02ec2ebafbc64a663fc4a15b22ea
irakligabunia <66872447+irakligabunia@users.noreply.github.com>
2020-11-07 13:43:06 +0400

    Update sh_flashlight.lua

--- gamemodes/nswarfare/plugins/flashlight/items/weapons/sh_flashlight.lua
+++ gamemodes/nswarfare/plugins/flashlight/items/weapons/sh_flashlight.lua
@@ -1,5 +1,5 @@
-﻿ITEM.name = "Ручной фонарь"
-ITEM.desc = "Для бойцов старой школы, очень прочный фонарь и, судя по потертостям, довольно-таки старый. \n\nХАРАКТЕРИСТИКИ: \n-технологическое приспособление \n-требует подзарядку"
+﻿ITEM.name = "Handheld flashlight"
+ITEM.desc = "A very practical, handheld torch with AA batteries"
 ITEM.price = 2806
 ITEM.exRender = false
 ITEM.weight = 0.48
