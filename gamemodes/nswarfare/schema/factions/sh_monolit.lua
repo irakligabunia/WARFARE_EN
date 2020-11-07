1f91dfa92d8f55cb4f0ce6e7c16a3614212769c1
irakligabunia <66872447+irakligabunia@users.noreply.github.com>
2020-11-07 12:16:14 +0400

    Update sh_monolit.lua

--- gamemodes/nswarfare/schema/factions/sh_monolit.lua
+++ gamemodes/nswarfare/schema/factions/sh_monolit.lua
@@ -1,5 +1,5 @@
-﻿FACTION.name = "Монолит"
-FACTION.desc = "Монолит — группировка, аналогичная, по сути, религиозной секте."
+﻿FACTION.name = "Monolith"
+FACTION.desc = "Monolith — A group of cultist-militants seeking to destroy the filth in the Zone."
 FACTION.color = Color(230, 230, 250)
 FACTION.isDefault = false
 FACTION.isPublic = true
