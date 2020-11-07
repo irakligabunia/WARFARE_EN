8701f24aaadadd461533bc849b4f998e61effc69
irakligabunia <66872447+irakligabunia@users.noreply.github.com>
2020-11-07 12:19:13 +0400

    Update sh_stalkers.lua

--- gamemodes/nswarfare/schema/factions/sh_stalkers.lua
+++ gamemodes/nswarfare/schema/factions/sh_stalkers.lua
@@ -1,5 +1,5 @@
-﻿FACTION.name = "Одиночки"
-FACTION.desc = "Одиночки"
+﻿FACTION.name = "Loners"
+FACTION.desc = "Loners"
 FACTION.color = Color(210, 105, 30)
 FACTION.models = {
 	"models/hdmodels/kek1ch/stalker_neutral_1_face_2.mdl",
