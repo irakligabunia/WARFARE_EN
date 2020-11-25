5d27c4828f68c8df1086ee7bc33a449bc3690d03
stalkerino <66872447+irakligabunia@users.noreply.github.com>
2020-11-26 02:47:35 +0400

    Delete sh_lastday.lua

--- gamemodes/nswarfare/schema/factions/sh_lastday.lua
+++
@@ -1,9 +0,0 @@
-FACTION.name = "Последний День"
-FACTION.desc = ""
-FACTION.color = Color(89, 53, 31)
-FACTION.models = {
-	"models/hdmodels/kek1ch/lastday_stalker_4_vadim.mdl"
-}
-FACTION.isDefault = false
-FACTION.isPublic = true
-FACTION_LASTDAY = FACTION.index
