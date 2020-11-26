a109d529e9d495329fdd0bdbf6932dbe495aafce
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 10:28:58 +0200

    Update sh_medal.lua

--- gamemodes/nswarfare/schema/items/base/sh_medal.lua
+++ gamemodes/nswarfare/schema/items/base/sh_medal.lua
@@ -1,5 +1,5 @@
-﻿ITEM.name = "Первенство"
-ITEM.desc = ""
+﻿ITEM.name = "Medal"
+ITEM.desc = "A medal given to champions."
 ITEM.price = 30000
 ITEM.model = "models/hardbass/money_10.mdl"
 ITEM.width = 1
