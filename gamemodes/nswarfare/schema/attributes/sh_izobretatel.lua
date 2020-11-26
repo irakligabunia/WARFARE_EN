31c03b6180904a53d5692ca55c32a02c642f72e5
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 09:06:37 +0200

    Update sh_izobretatel.lua

--- gamemodes/nswarfare/schema/attributes/sh_izobretatel.lua
+++ gamemodes/nswarfare/schema/attributes/sh_izobretatel.lua
@@ -1,2 +1,2 @@
 ATTRIBUTE.name = "Attribute: Inventor"
-ATTRIBUTE.desc = "As a child, you collected the radio from old parts in your father's drawer, in your youth repaired your motorcycle with parts from a flea market, and now feel that you will find a way to implement your skills with the technical garbage in the Exclusion zone."
+ATTRIBUTE.desc = "As a child, you collected the radio from old parts in your father's drawer, in your youth you repaired your motorcycle with parts from a flea market, and now feel that you will find a way to implement your skills with the technical garbage in the Exclusion zone."
