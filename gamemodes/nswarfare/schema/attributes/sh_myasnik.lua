45a7a90fb81cb8048dedd2c40d0705b0a6950973
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 09:20:10 +0200

    Update sh_myasnik.lua

--- gamemodes/nswarfare/schema/attributes/sh_myasnik.lua
+++ gamemodes/nswarfare/schema/attributes/sh_myasnik.lua
@@ -1,2 +1,2 @@
-ATTRIBUTE.name = "Навык: Мясник"
-ATTRIBUTE.desc = "Вы буквально всега вселяете страх в тварей Зоны. И неудевительно, ведь в большинстве случаев Вы - последнее, что они видят в своей жизни. Узнав, как вы расправляетесь с тушей мутанта после, вас начинают боятся даже люди."
+ATTRIBUTE.name = "Attribute: Butcher"
+ATTRIBUTE.desc = "You instill fear in the mutants of the zone. Not because you're dangerous, but in most cases because you are the last thing they see in their lives. After humans learn how you deal with the carcass of a mutant, even they begin to fear you."
