46f9d9797955f940a180a797f2b0d59122acf86f
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 13:17:10 +0200

    Update sh_aps.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_aps.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_aps.lua
@@ -1,9 +1,9 @@
-﻿ITEM.name = "АПС (заказной)"
-ITEM.desc = "Автоматический пистолет Стечкина. Модификация под новый усиленный патрон 9×18 мм (9х18 мм +P+). Является самым распространённым пистолетом в Зоне."
+﻿ITEM.name = "APS"
+ITEM.desc = "Stechkin's automatic pistol. Modification for the new reinforced cartridge 9×18 mm (9 x18 mm zP). It is the most common gun in the zone."
 ITEM.price = 18700
 ITEM.class = "weapon_cop_aps"
 ITEM.weaponCategory = "2"
-ITEM.category = "Оружие"
+ITEM.category = "Weapon"
 ITEM.exRender = false
 ITEM.weight = 0.81
 ITEM.model = "models/wick/weapons/stalker/stcopwep/w_aps_model.mdl"
