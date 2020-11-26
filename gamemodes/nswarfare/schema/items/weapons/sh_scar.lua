bf239e67f6b93f9f3260e348db8e3824b6cb921e
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 14:13:50 +0200

    Update sh_scar.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_scar.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_scar.lua
@@ -1,9 +1,9 @@
-﻿ITEM.name = "FN SCAR-H (заказной)"
-ITEM.desc = "это не просто винтовка, ее можно назвать настоящим модульным стрелковым комплексом, который включает в себя две базовые модификации. \n\nБоеприпасы 5,56x45 мм"
+﻿ITEM.name = "FN SCAR-H"
+ITEM.desc = "A modern, high quality customizable assault rifle. Perfect for medium range combat. \n\nAmmunition 5,56x45 мм"
 ITEM.price = 97952
 ITEM.class = "weapon_cop_scar"
 ITEM.weaponCategory = "1"
-ITEM.category = "Оружие"
+ITEM.category = "Weapon"
 ITEM.exRender = false
 ITEM.model = "models/wick/weapons/stalker/stcopwep/w_scar_model_stcop.mdl"
 ITEM.width = 6
