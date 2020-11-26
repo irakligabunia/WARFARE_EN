9a0c1601fa59cb144b9460ee0becdde46d22aac8
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 13:48:50 +0200

    Update sh_hpsa.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_hpsa.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_hpsa.lua
@@ -1,9 +1,9 @@
 ﻿ITEM.name = "Browing Hi-Power"
-ITEM.desc = "Модифицированная модель пистолета первой половины 20-ого века, которая такая же надежная, точная и мощна, как оригинальный образец. Эта модель получила репутацию одного из лучших пистолетов 20-ого века и пользуется большой популярностью среди сталкеров. \n\nБоеприпасы 9х19"
+ITEM.desc = "A modified model of the first half of the 20th century pistol, which is as reliable, accurate and powerful as the original model. This model has gained a reputation as one of the best pistols of the 20th century and is very popular among stalkers. \n\nAmmunition 9х19"
 ITEM.price = 7749
 ITEM.class = "weapon_cop_hpsa"
 ITEM.weaponCategory = "2"
-ITEM.category = "Оружие"
+ITEM.category = "Weapon"
 ITEM.exRender = false
 ITEM.model = "models/wick/weapons/stalker/stcopwep/w_wpn_hpsa_stcop.mdl"
 ITEM.width = 2
