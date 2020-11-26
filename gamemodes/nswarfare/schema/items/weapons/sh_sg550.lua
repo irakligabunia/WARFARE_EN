db2334fce073ffa5bb06994e4f387a4d9722ba19
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 14:14:49 +0200

    Update sh_sg550.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_sg550.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_sg550.lua
@@ -1,9 +1,9 @@
 ﻿ITEM.name = "SG 550"
-ITEM.desc = "Одна из относительно новых швейцарских штурмовых винтовок под патрон 5,56×45 мм. Точность, мягкая отдача и высокая надёжность данного оружия быстро завоевали ему самую широкую популярность. \n\nБоеприпасы 5.56x45"
+ITEM.desc = "One of the relatively new Swiss assault rifles under the cartridge 5.56×45 mm. Precision, soft recoil and high reliability of this weapon quickly is the reason for its wide popularity. \n\nAmmunition 5.56x45"
 ITEM.price = 44612
 ITEM.class = "weapon_cop_sig550"
 ITEM.weaponCategory = "1"
-ITEM.category = "Оружие"
+ITEM.category = "Weapon"
 ITEM.exRender = false
 ITEM.model = "models/wick/weapons/stalker/stcopwep/w_sig550_model_stcop.mdl"
 ITEM.width = 5
