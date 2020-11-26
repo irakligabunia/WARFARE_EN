a7316cb068273e1c6051c593323623ebe5716865
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 14:01:59 +0200

    Update sh_lr300.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_lr300.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_lr300.lua
@@ -1,9 +1,9 @@
 ﻿ITEM.name = "LR-300-ML"
-ITEM.desc = "Новая ступень в развитии знаменитой линейки оружия. Фактически изменения коснулись лишь верхней половины ствольной коробки со стволом, газоотводным узлом и затворной группой, в результате чего повысились эргономичность и надёжность, а масса, наоборот, снизилась. \n\nБоеприпасы 5.56x45"
+ITEM.desc = "A new step in the development of the famous line of weapons. In fact, the changes affected only the top half of the upper receiver, the gas bolt and the reclusive group, which resulted in an increase in ergonomics and reliability, and the mass, on the contrary, decreased. \n\nAmmunition 5.56x45"
 ITEM.price = 40316
 ITEM.class = "weapon_cop_lr300"
 ITEM.weaponCategory = "1"
-ITEM.category = "Оружие"
+ITEM.category = "Weapon"
 ITEM.exRender = false
 ITEM.model = "models/wick/weapons/stalker/stcopwep/w_lr300_model_stcop.mdl"
 ITEM.width = 4
