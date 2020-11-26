f721ee3154018b14a1d046d3b554cfd629e0b33c
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 13:35:07 +0200

    Update sh_fnfs2000.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_fnfs2000.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_fnfs2000.lua
@@ -1,10 +1,10 @@
 ITEM.name = "FN F2000"
-ITEM.desc = "Бельгийский автоматно-гранатомётный комплекс, выполненный в компоновке «булл-пап». Оснащён компьютеризованным прицелом и подствольным гранатомётом. Встречается только у ветеранов и мастеров в экзоскелетах. \n\nБоеприпасы 5.56х45"
+ITEM.desc = "Belgian bullpup equipped with a computerized sight and a sub-barrel grenade launcher. Is seen only with veterans and masters in exoskeletons. \n\nAmmunition 5.56х45"
 ITEM.price = 66112
 ITEM.model = "models/wick/weapons/stalker/stcopwep/w_fn2000_model_stcop.mdl"
 ITEM.class = "weapon_cop_fn2000"
 ITEM.weaponCategory = "1"
-ITEM.category = "Оружие"
+ITEM.category = "Weapon"
 ITEM.height = 2
 ITEM.width = 4
 ITEM.exRender = false
