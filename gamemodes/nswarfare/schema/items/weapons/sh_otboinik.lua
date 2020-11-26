c6bc97033a7c89f03766525ea9e22effe01619a2
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 14:05:23 +0200

    Update sh_otboinik.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_otboinik.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_otboinik.lua
@@ -1,9 +1,9 @@
-﻿ITEM.name = "Armsel Protecta (заказной)"
-ITEM.desc = "Гладкоствольный боевой дробовик, разработанный для повышения огневой мощи спецподразделений армии и полиции. Основные достоинства — высокие ёмкость магазина и практическая скорострельность; основные недостатки — внушительные габариты и масса оружия, а также невысокая скорость перезарядки. В Зоне встречается редко и применяется в основном для зачистки логов мутантов. \n\nБоеприпасы 12х70"
+﻿ITEM.name = "Armsel Protecta"
+ITEM.desc = "Smooth-bore shotgun designed to increase the firepower of special forces of the army and police. The main advantages are high capacity of the store and practical rate of fire; the main drawbacks are the impressive size and weight of the weapon, as well as the low speed of reloading. The zone is rare and is used mainly to strip the dens of mutants. \n\nAmmunition 12х70"
 ITEM.price = 56692
 ITEM.class = "weapon_cop_protecta"
 ITEM.weaponCategory = "1"
-ITEM.category = "Оружие"
+ITEM.category = "Weapon"
 ITEM.exRender = false
 ITEM.model = "models/wick/weapons/stalker/stcopwep/w_protecta_model_stcop.mdl"
 ITEM.width = 4
