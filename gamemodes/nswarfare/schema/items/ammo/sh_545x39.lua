c76bfbd4793145661537b8cf724783a56ee5d9ce
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 10:17:11 +0200

    Update sh_545x39.lua

--- gamemodes/nswarfare/schema/items/ammo/sh_545x39.lua
+++ gamemodes/nswarfare/schema/items/ammo/sh_545x39.lua
@@ -1,7 +1,7 @@
 ﻿ITEM.name = "5,45x39 Rounds"
 ITEM.ammo = "545x39"
 ITEM.ammoAmount = 120
-ITEM.desc = "Box with 60 rounds of 5,45x39"
+ITEM.desc = "Box with 120 rounds of 5,45x39"
 ITEM.category = "Ammunition"
 ITEM.price = 3400
 ITEM.exRender = false
