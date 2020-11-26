7a0be5f67fdfb63b5bd4e45333864b011c509ab0
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 13:19:03 +0200

    Update sh_asval.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_asval.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_asval.lua
@@ -1,5 +1,5 @@
 ﻿ITEM.name = "AS Val"
-ITEM.desc = "These weapons were created on the basis of a special sniper rifle of the VSS as a silent weapon for special forces troops. It allows the shooter to fire effectively at the enemy's body armor without demarking his position. One of the main features is the use of an integrated silencer and 9 x39 mm caliber cartridges \n\nAmmunition 9x39"
+ITEM.desc = "This weapon were created on the basis of a special sniper rifle of the VSS as a silent weapon for special forces troops. It allows the shooter to fire effectively at the enemy's body armor without demarking his position. One of the main features is the use of an integrated silencer and 9 x39 mm caliber cartridges \n\nAmmunition 9x39"
 ITEM.price = 72952
 ITEM.class = "weapon_cop_val"
 ITEM.weaponCategory = "1.5"
