b51466c15e0b69ba544465932314cf64e56266f0
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 13:31:12 +0200

    Update sh_cheizer.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_cheizer.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_cheizer.lua
@@ -1,9 +1,9 @@
 ﻿ITEM.name = "MP153"
-ITEM.desc = "Российское самозарядное гладкоствольное ружьё, в народе называемое «мурка», которое применяется для различных видов охоты, спортивной стрельбы, охраны правопорядка и самообороны. \n\nБоеприпасы 12х70"
+ITEM.desc = "Russian self-loading smooth-bore shotgun, locally called 'murka', which is used for various types of hunting, sports shooting, law enforcement and self-defense. \n\nAmmunition 12х70"
 ITEM.price = 18066
 ITEM.class = "weapon_cop_winchester1300"
 ITEM.weaponCategory = "1"
-ITEM.category = "Оружие"
+ITEM.category = "Weapon"
 ITEM.exRender = false
 ITEM.model = "models/wick/weapons/stalker/stcopwep/w_mp153_model.mdl"
 ITEM.width = 5
