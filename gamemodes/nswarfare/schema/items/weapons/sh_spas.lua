86a41ad4d3f771f467bd7c1f8d3c0d3759eb80b5
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 14:15:48 +0200

    Update sh_spas.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_spas.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_spas.lua
@@ -1,9 +1,9 @@
 ﻿ITEM.name = "SPAS-12"
-ITEM.desc = "Гладкоствольное самозарядное ружьё, разработанное в конце 1970-х в качестве универсального боевого оружия для полиции и штурмовых подразделений армии. Отличается высокой надёжностью и гибкостью применения; пожалуй, лучшего ствола для ближнего боя в Зоне не найти. С другой стороны, масса этого оружия довольно велика, устройство сложно, а цена высока. \n\nБоеприпасы 12х70"
+ITEM.desc = "A smooth-bore self-loading shotgun developed in the late 1970s as a versatile combat weapon for the police and assault units of the army. It is highly reliable and flexible; perhaps the best barrel for close quarter combat in the Zone. On the other hand, the mass of these weapons is quite large, the device is difficult, and the price is high. \n\nAmmunition 12х70"
 ITEM.price = 26960
 ITEM.class = "weapon_cop_spas12"
 ITEM.weaponCategory = "1"
-ITEM.category = "Оружие"
+ITEM.category = "Weapon"
 ITEM.exRender = false
 ITEM.model = "models/wick/weapons/stalker/stcopwep/w_spas_model_stcop.mdl"
 ITEM.width = 5
