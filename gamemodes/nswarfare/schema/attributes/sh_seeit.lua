78c34bfc76c35d8f629f5d7478bc017348dd7716
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 09:22:23 +0200

    Update sh_seeit.lua

--- gamemodes/nswarfare/schema/attributes/sh_seeit.lua
+++ gamemodes/nswarfare/schema/attributes/sh_seeit.lua
@@ -1,2 +1,2 @@
-ATTRIBUTE.name = "Навык: Внимательность"
-ATTRIBUTE.desc = "Вы приучены подмечать самые незначительные детали и не только в поведении людей, но и в окружающей обстановке, а уж при обыске от вас не спрячется ни одна копейка, ни один болт, ни один человек..."
+ATTRIBUTE.name = "Attribute: Awareness"
+ATTRIBUTE.desc = "You are accustomed to notice the most insignificant details and not only in the behavior of people, but also in the environment, and even when you search not a single penny, not a single bolt, not a single person can hide from you..."
