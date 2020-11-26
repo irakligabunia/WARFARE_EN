865b645445be6087702d6409ad064ce3478a3f97
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 10:21:00 +0200

    Update sh_9x19.lua

--- gamemodes/nswarfare/schema/items/ammo/sh_9x19.lua
+++ gamemodes/nswarfare/schema/items/ammo/sh_9x19.lua
@@ -1,8 +1,8 @@
-ITEM.name = "Патроны 9x19"
+ITEM.name = "9x19 Rounds"
 ITEM.ammo = "9x19"
 ITEM.ammoAmount = 60
-ITEM.desc = "Коробка с патронами калибра 9x19"
-ITEM.category = "Патроны"
+ITEM.desc = "Box with 60 rounds of 9x19"
+ITEM.category = "Ammunition"
 ITEM.price = 2900
 ITEM.exRender = false
 
