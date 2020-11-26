eac4bb5a67e493cae14eb0e21e527e3f051885c5
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 09:35:44 +0200

    Update sh_merchants.lua

--- gamemodes/nswarfare/schema/factions/sh_merchants.lua
+++ gamemodes/nswarfare/schema/factions/sh_merchants.lua
@@ -1,5 +1,5 @@
 FACTION.name = "Traders"
-FACTION.desc = ""
+FACTION.desc = "A barge with a beating heart. They move, control and exploit the economy of the Zone."
 FACTION.color = Color(255, 255, 0)
 FACTION.models = {
 	"models/hdmodels/kek1ch/stalker_neutrala1.mdl"
