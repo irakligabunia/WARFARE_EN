fa5725dd09e0ede3e50f7630eb195ee7165d49f2
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 14:25:46 +0200

    Update sh_walter.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_walter.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_walter.lua
@@ -1,9 +1,9 @@
 ﻿ITEM.name = "Walter P99"
-ITEM.desc = "Пистолет немецкого производства под патрон 9х19. Неплохая точность и вместительный магазин делают его эффективным оружием в умелых руках. \n\nБоеприпасы 9x19"
+ITEM.desc = "German-made pistol under the cartridge 9 x19. Good accuracy and roomy store make it an effective weapon in capable hands. \n\nAmmunition 9x19"
 ITEM.price = 8040
 ITEM.class = "weapon_cop_walther"
 ITEM.weaponCategory = "2"
-ITEM.category = "Оружие"
+ITEM.category = "Weapon"
 ITEM.exRender = false
 ITEM.model = "models/wick/weapons/stalker/stcopwep/w_p99_model.mdl"
 ITEM.width = 2
