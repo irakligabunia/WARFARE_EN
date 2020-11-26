6431269ad00569b1e3db288e92b76c6ca10bc43a
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 09:01:07 +0200

    Update sh_equipdop.lua

--- gamemodes/nswarfare/schema/attributes/sh_equipdop.lua
+++ gamemodes/nswarfare/schema/attributes/sh_equipdop.lua
@@ -1,2 +1,2 @@
-ATTRIBUTE.name = "Дополнительный набор оружия"
-ATTRIBUTE.desc = "Надежное и хорошо зарекомендовавшее себя оружие в условиях ЧЗО. Старый-добрый АКСУ, патроны к нему, гранаты и добротный нож - вполне универсальный набор против любой живой угрозы."
+ATTRIBUTE.name = "Additional weapons"
+ATTRIBUTE.desc = "Reliable and well-proven weapons in the conditions of the Chernobyl Exclusion Zone. The good old AKSU, ammunition to it, grenades and a good knife - quite a universal set against any living threat."
