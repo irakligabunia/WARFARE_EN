6217511563a55a1a0b0c8694527cad0005132eb4
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 14:08:24 +0200

    Update sh_pb.lua

--- gamemodes/nswarfare/schema/items/weapons/sh_pb.lua
+++ gamemodes/nswarfare/schema/items/weapons/sh_pb.lua
@@ -1,9 +1,9 @@
-﻿ITEM.name = "ПБ"
-ITEM.desc = "Пистолет был разработан во второй половине ХХ века на базе пистолета ПМм для вооружения разведывательных групп и персонала спецслужб. Данная модель считается морально устаревшей, но в умелых руках это по-прежнему весьма эффективное оружие. \n\nБоеприпасы 9х18"
+﻿ITEM.name = "PB"
+ITEM.desc = "The pistol was developed in the second half of the 20th century on the basis of the PMM pistol for arming intelligence groups and intelligence personnel. This model is considered obsolete, but in the right hands it is still a very effective weapon. \n\nAmmunition 9х18"
 ITEM.price = 8476
 ITEM.class = "weapon_cop_pb"
 ITEM.weaponCategory = "2"
-ITEM.category = "Оружие"
+ITEM.category = "Weapon"
 ITEM.exRender = false
 ITEM.weight = 1.03
 
