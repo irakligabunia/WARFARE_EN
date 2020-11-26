4dabc7c3601508f8655a4c825f025432bab5afb7
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-26 09:05:40 +0200

    Update sh_izobretatel.lua

--- gamemodes/nswarfare/schema/attributes/sh_izobretatel.lua
+++ gamemodes/nswarfare/schema/attributes/sh_izobretatel.lua
@@ -1,2 +1,2 @@
-ATTRIBUTE.name = "Навык: Изобретатель"
-ATTRIBUTE.desc = "В детстве вы собрали радио из старых деталей в ящике отца, в юности чинили свой мотоцикл запчастями с барахолки, а теперь чувствуете, что обязательно найдете пирменение техническому мусору в Зоне Отчуждения."
+ATTRIBUTE.name = "Attribute: Inventor"
+ATTRIBUTE.desc = "As a child, you collected the radio from old parts in your father's drawer, in your youth repaired your motorcycle with parts from a flea market, and now feel that you will find a way to implement your skills with the technical garbage in the Exclusion zone."
