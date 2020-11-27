97d6297742551557cd95b77a1e3eaedf79812f5f
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-27 09:58:15 +0200

    Update sh_cassette_standart.lua

--- gamemodes/nswarfare/plugins/cassette_player/items/cassette/sh_cassette_standart.lua
+++ gamemodes/nswarfare/plugins/cassette_player/items/cassette/sh_cassette_standart.lua
@@ -1,5 +1,5 @@
-﻿ITEM.name = "Обычная Кассета"
-ITEM.desc = "На нее записана распространенная и приятная музыка, подойдет для большинства слушателей, содержит большое количество миксов."
+﻿ITEM.name = "Regular Cassette"
+ITEM.desc = "Recordings of common and pleasant music, suitable for most listeners, contains a large number of mixes."
 ITEM.price = 27940
 ITEM.model = "models/kek1ch/cassette_orange.mdl"
 
