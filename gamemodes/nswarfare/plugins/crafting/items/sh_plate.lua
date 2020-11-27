56d03ca08ab9e1076e51e3602f7d50156a1cb568
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-28 00:49:10 +0200

    Update sh_plate.lua

--- gamemodes/nswarfare/plugins/crafting/items/sh_plate.lua
+++ gamemodes/nswarfare/plugins/crafting/items/sh_plate.lua
@@ -1,5 +1,5 @@
-﻿ITEM.name = "Стальные пластины"
-ITEM.desc = "Специально разработанные пластины, изготавливаются путем длительной и сложной термообработки стали. Производятся с учетом баллистических свойств брони и установки различных модификаций. Используются для гашения ударов на передней и задней части туловища. \n\nХАРАКТЕРИСТИКИ: \n-военное изделие \n-используется для крафта"
+﻿ITEM.name = "Steel plates"
+ITEM.desc = "Specially designed plates are made by long and complex steel treatment. Manufactured with the ballistic properties of armor and installation of various modifications. Used to extinguish strokes on the front and back of the torso. \n\nATTRIBUTES: \n-Military grade \n-Crafting device"
 ITEM.price = 9432
 ITEM.exRender = false
 ITEM.weight = 8.29
