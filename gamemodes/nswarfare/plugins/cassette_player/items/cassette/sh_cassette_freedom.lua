9e98c75d89927cc8ce1ecd4176852a7a986e0c75
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-27 09:55:11 +0200

    Update sh_cassette_freedom.lua

--- gamemodes/nswarfare/plugins/cassette_player/items/cassette/sh_cassette_freedom.lua
+++ gamemodes/nswarfare/plugins/cassette_player/items/cassette/sh_cassette_freedom.lua
@@ -1,5 +1,5 @@
-﻿ITEM.name = "Кассета Свободы"
-ITEM.desc = "Список треков:\n1. Джайа - Малиновые пальмы\n2. Тема свободы"
+﻿ITEM.name = "Freedom Cassette"
+ITEM.desc = "Список треков:\n1. Jaya - Raspberry Palms\n2. Freedom theme"
 ITEM.price = 44240
 ITEM.model = "models/kek1ch/cassette_red1.mdl"
 
