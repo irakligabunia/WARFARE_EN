164ed06fb7e5ee8486fe48640c3a88adfadf2b00
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-28 00:45:54 +0200

    Update sh_nitka_sintetichesk.lua

--- gamemodes/nswarfare/plugins/crafting/items/sh_nitka_sintetichesk.lua
+++ gamemodes/nswarfare/plugins/crafting/items/sh_nitka_sintetichesk.lua
@@ -1,5 +1,5 @@
-﻿ITEM.name = "Плотная синтетическая нить"
-ITEM.desc = "Катушка с крепкой синтетической нитью для шиться, осталось совсем немного, но вполне хватит, чтоб залатать пару дыр. Незаменимая вещь в этих краях, так как такая нить, в отличии от обычной, выдер \n\nХАРАКТЕРИСТИКИ:\n-технологическое приспособление\n-низкая ценность"
+﻿ITEM.name = "Dense synthetic thread"
+ITEM.desc = "A coil with a strong synthetic thread for sewing, there is not much left, but enough to patch a couple of holes. An irreplaceable thing in these parts \n\nATTRIBUTES:\n-Technological device\n-Low value"
 ITEM.price = 320	
 ITEM.weight = 0.02
 
