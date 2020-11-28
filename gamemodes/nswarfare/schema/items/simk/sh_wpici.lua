ed67f9a955acb1652fd7f5172d49c9269e9bb2c0
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-28 17:52:16 +0200

    Update sh_wpici.lua

--- gamemodes/nswarfare/schema/items/simk/sh_wpici.lua
+++ gamemodes/nswarfare/schema/items/simk/sh_wpici.lua
@@ -1,6 +1,6 @@
 ﻿ITEM.name = "Artefact tools"
-ITEM.desc = "Tools, made of hardened steel, designed directly to work with artifacts. All types of artifacts are used in conjunction with containers, which makes it easier to manipulate them. No stalker leaves the base without such a tool - it proves the importance of such a tool, when you do not want to pick up an extra dose of radiation coming from the artifact.  \n\nХАРАКТЕРИСТИКИ: \n-технологическое приспособление \n-используется при работе с артефактами \n-имеет важное значение при работе с контейнерами"
-ITEM.category = "misc"
+ITEM.desc = "Tools, made of hardened steel, designed directly to work with artifacts. All types of artifacts are used in conjunction with containers, which makes it easier to manipulate them. No stalker leaves the base without such a tool - it proves the importance of such a tool, when you do not want to pick up an extra dose of radiation coming from the artifact.  \n\nATTRIBUTES: \n-Technological device \n-Usages - handling artefacts \n-Is important when dealing with containers"
+ITEM.category = "Misc"
 ITEM.price = 421
 ITEM.exRender = false
 ITEM.model = "models/hardbass/kowel.mdl"
