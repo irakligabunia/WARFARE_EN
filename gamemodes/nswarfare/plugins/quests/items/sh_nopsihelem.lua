47170d2862a254767be2e9811fd07a34d2c7cbdc
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-28 15:52:32 +0200

    Update sh_nopsihelem.lua

--- gamemodes/nswarfare/plugins/quests/items/sh_nopsihelem.lua
+++ gamemodes/nswarfare/plugins/quests/items/sh_nopsihelem.lua
@@ -1,5 +1,5 @@
 ﻿ITEM.name = "Untuned Psi helmet"
-ITEM.desc = "A prototype developed by scientists to protect against psi radiation. It is a headphones with filling and mesh covering the head. It's dimensions allow you to wear it under the hood, helmet, helmet and other equipment. \n\nATTRIBUTES: \n-Important item \n-Equipment \n-Does not provide protection from psi-radiation sources"
+ITEM.desc = "A prototype developed by scientists to protect against psi radiation. It is headphones are filled with mesh, that continues to cover the entire head. Dimensions allow you to wear it under the hood, helmet, helmet and other equipment. \n\nATTRIBUTES: \n-Important item \n-Equipment \n-Does not provide protection from psi-radiation sources"
 ITEM.price = 12900
 ITEM.exRender = false
 ITEM.weight = 0.12
