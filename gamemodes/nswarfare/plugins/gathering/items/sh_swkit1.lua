3b3d00407768423d45a5df6a9042666c78bd7fc5
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-11-28 02:45:32 +0200

    Update sh_swkit1.lua

--- gamemodes/nswarfare/plugins/gathering/items/sh_swkit1.lua
+++ gamemodes/nswarfare/plugins/gathering/items/sh_swkit1.lua
@@ -1,5 +1,5 @@
-﻿ITEM.name = "Универсальный швейный набор"
-ITEM.desc = "Данный универсальный швейный набор содержит большое качественной фурнитуры для шитья, чем простые комплекты. Две катушки нейлоновых нитей и большой выбор качественных иголок делают его пригодным для работы с толстыми и твердыми материалами, в том числе с легкими шкурами мутантов и сложными искусственными плотными тканями. \n\nХАРАКТЕРИСТИКИ: \n-технологическое приспособление" --Его описание
+﻿ITEM.name = "Universal sewing kit"
+ITEM.desc = "This versatile sewing kit contains more quality sewing fittings than simple kits. Two coils of nylon filaments and a large selection of quality needles make it suitable for working with thick and hard materials, including light mutant skins and complex artificial dense tissues. \n\nATTRIBUTES: \n-Technical device" --Его описание
 ITEM.price = 2763
 ITEM.exRender = true
 ITEM.weight = 0.29
