8d557a0803746ffc3e24d32139da161029bfeb00
stalkerino <66872447+irakligabunia@users.noreply.github.com>
2020-11-28 08:46:03 +0400

    Update sh_plate.lua

--- gamemodes/nswarfare/plugins/crafting/items/sh_plate.lua
+++ gamemodes/nswarfare/plugins/crafting/items/sh_plate.lua
@@ -1,6 +1,6 @@
 ﻿ITEM.name = "Steel plates"
 ITEM.desc = "Specially designed plates are made by long and complex steel treatment. Manufactured with the ballistic properties of armor and installation of various modifications. Used to extinguish strokes on the front and back of the torso. \n\nATTRIBUTES: \n-Military grade \n-Crafting device"
-ITEM.price = 9432
+ITEM.price = 4155
 ITEM.exRender = false
 ITEM.weight = 8.29
 
