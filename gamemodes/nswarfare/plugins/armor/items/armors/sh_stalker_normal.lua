8228d34632b3e88b8c61be4eee2e2a44da0662c1
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-12-05 17:13:38 +0200

    Update sh_stalker_normal.lua

--- gamemodes/nswarfare/plugins/armor/items/armors/sh_stalker_normal.lua
+++ gamemodes/nswarfare/plugins/armor/items/armors/sh_stalker_normal.lua
@@ -1,4 +1,4 @@
-﻿ITEM.name = "Combat Suit «Sunrise»"
+﻿ITEM.name = "Sunrise suit"
 ITEM.desc = "The most common suit in the Zone, coated in kevlar and ceramic plates although no protection from anomalies"
 ITEM.price = 21006
 ITEM.ric = 20
@@ -102,9 +102,6 @@ ITEM.ArmorFade[FACTION_RENEGAT] = {Models = {
 ITEM.ArmorFade[FACTION_NATO] = {Models = {
 "models/spenser/kek1ch/nato_soldier_spetsnaz_vadim.mdl"
 }}
-ITEM.ArmorFade[FACTION_LASTDAY] = {Models = {
-"models/spenser/kek1ch/lastday_stalker_1_vadim.mdl"
-}}
 
 local DuseArmor = {
 	"kevlar",
@@ -115,11 +112,11 @@ local DuseArmor = {
 }
 
 ITEM.functions.duse = {
-	name = "разобрать",
+	name = "disassemble",
 	onRun = function(item)
 		if (IsValid(item.player) && item.player:Alive()) then
 			if not item.player:getChar():getInv():hasItem("crowboar") then
-				item.player:notify("У вас нет лома!")
+				item.player:notify("You don't have a crowbar!")
 				return false
 			end
 			
