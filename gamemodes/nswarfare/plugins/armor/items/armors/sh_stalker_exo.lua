b96e252a2bef5ae536052094d2c598a61c04cf02
psywarhehim <75049298+psywarhehim@users.noreply.github.com>
2020-12-05 17:13:03 +0200

    Update sh_stalker_exo.lua

--- gamemodes/nswarfare/plugins/armor/items/armors/sh_stalker_exo.lua
+++ gamemodes/nswarfare/plugins/armor/items/armors/sh_stalker_exo.lua
@@ -54,9 +54,6 @@ ITEM.ArmorFade[FACTION_RENEGAT] = {Models = {
 ITEM.ArmorFade[FACTION_NATO] = {Models = {
 "models/spenser/kek1ch/nato_exo_vadim.mdl"
 }}
-ITEM.ArmorFade[FACTION_LASTDAY] = {Models = {
-"models/spenser/kek1ch/lastday_stalker_exo_vadim.mdl"
-}}
 
 local DuseArmor = {
 	"plate",
@@ -67,11 +64,11 @@ local DuseArmor = {
 }
 
 ITEM.functions.duse = {
-	name = "Разобрать",
+	name = "disassemble",
 	onRun = function(item)
 		if (IsValid(item.player) && item.player:Alive()) then
 			if not item.player:getChar():getInv():hasItem("crowboar") then
-				item.player:notify("У вас нет лома!")
+				item.player:notify("You don't have a crowbar!")
 				return false
 			end
 
